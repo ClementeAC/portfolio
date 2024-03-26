48be0c0f7428eff57c88d5d8883432a2feda344c
Clemente Castejon <clementecastejon3@gmail.com>
2024-03-26 06:55:41 -0400

    fixed typo

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         &copy; 2024 Clemente Castejon. All rights reserved.
       </small>
       <p className="text-xs ">
-        <span className="font-semibold">About this website: </span> build with
+        <span className="font-semibold">About this website: </span> built with
         React & Next.js (App Router & Server Actions), Typescript, Tailwind CSS,
         Framer Motion, React Email & Resend, Vercel Hosting.
       </p>
